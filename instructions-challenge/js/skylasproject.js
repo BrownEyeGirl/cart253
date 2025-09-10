c139a14fadbdacdae9602538c3797a1a86d8985c
Skyla Trousdale <skylatrousdale@gmail.com>
2025-09-10 19:02:43 -0400

    Canvas created, sky set

---
+++ instructions-challenge/js/skylasproject.js
@@ -0,0 +1,29 @@
+/**
+ * Dream Landscape 
+ * Skyla Trousdale 
+ * 
+ * This is the drawing js code for the landscape. 
+ */
+
+"use strict";
+
+/**
+ * Sets sky
+*/
+function setup() {
+    createCanvas(500,500);
+    background("#D4F0F0"); 
+}
+
+
+/**
+ * Draws clouds, 
+*/
+function draw() {
+
+}
+
+
+/**
+ * Clouds
+ */
