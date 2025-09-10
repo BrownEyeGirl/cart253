e119a930c81bbb3c980161a04f50754c41c2ad02
Skyla Trousdale <skylatrousdale@gmail.com>
2025-09-10 19:50:04 -0400

    final clouds

--- instructions-challenge/js/skylasproject.js
+++ instructions-challenge/js/skylasproject.js
@@ -31,13 +31,18 @@ function draw() {
 function clouds() {
     fill(255); 
     noStroke();
+
+    fill("ffffe6");
+    ellipse(80,55,50,50);
     fill("#ffffcc");
     ellipse(80, 55, 40, 40); // sun 
+    fill("#ffeecc");
+    ellipse(80, 55, 30, 30);
 
     // cloud shadow
     fill(230);
-    ellipse(110, 110, 50, 50); // base cloud
-    ellipse(110, 180, 20, 20) // centre clouds
+    ellipse(110, 110, 50, 50);
+    ellipse(110, 180, 20, 20);
 
     ellipse(90, 150, 60, 60); 
     ellipse(130, 150, 60, 60);
@@ -48,8 +53,8 @@ function clouds() {
 
     // cloud 1
     fill(255);
-    ellipse(100, 100, 50, 50); // base cloud
-    ellipse(100, 170, 20, 20) // centre clouds
+    ellipse(100, 100, 50, 50); 
+    ellipse(100, 170, 20, 20);
 
     ellipse(80, 140, 60, 60); 
     ellipse(120, 140, 60, 60);
@@ -60,8 +65,8 @@ function clouds() {
 
     // cloud 2 shadow 
     fill(230);
-    ellipse(410, 210, 50, 50); // base cloud
-    ellipse(410, 280, 20, 20) // centre clouds
+    ellipse(410, 210, 50, 50); 
+    ellipse(410, 280, 20, 20);
 
     ellipse(390, 250, 60, 60); 
     ellipse(430, 250, 60, 60);
@@ -72,8 +77,8 @@ function clouds() {
 
     // cloud 2 
     fill(255);
-    ellipse(410, 200, 50, 50); // base cloud
-    ellipse(410, 270, 20, 20) // centre clouds
+    ellipse(410, 200, 50, 50); 
+    ellipse(410, 270, 20, 20);
 
     ellipse(390, 240, 60, 60); 
     ellipse(430, 240, 60, 60);
