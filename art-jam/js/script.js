c138e425cdcd97a64c9f3404e2ed96c27d5035a0
Skyla Trousdale <skylatrousdale@gmail.com>
2025-09-30 16:22:33 -0400

    selfie added to art jam (working on visuals)

--- art-jam/js/script.js
+++ art-jam/js/script.js
@@ -4,26 +4,91 @@
  * 
  * An ultra simple example of instructions
  */
+/**
+ 
+ * Skyla Trousdale
+ * 
+ */
 
 "use strict";
-let ellipseSize = 40; 
 
 /**
- * Creates the canvas
-*/
+
+let h; 
+let w; 
+
+let col = {
+    r: 255, 
+    g: 55, 
+    b: 0
+}
+
 function setup() {
-    createCanvas(400, 400);
+
+    w = 600; 
+    h = 400; 
+    createCanvas(w, h);
+    
+
+}
+
+let img;
+
+function preload() {
+    img = loadImage('assets/images/trees.jpg');
 }
 
 
 /**
- * Sets background, draws the eye
-*/
+ * Draw symetrical circles (reflected on y)
+
 function draw() {
-    background(100); 
-    push(); 
-    full(255, 0, 0);
-    ellipse(width/2, height/2, ellipseSize, ellipseSize); // height, width, built in variables that define the size of screen. 
-    ellipse(mouseX, mouseY, ellipseSize, ellipseSize); 
-    pop(); 
+    background(200);
+    text("The image would be loaded below:", 20, 20);
+    image(img, 20, 40, 200,200);
+
+}
+
+*/
+
+
+let img;
+let w;
+let h;
+let count; 
+
+
+function preload() {
+  img = loadImage('assets/images/spooky.jpg');
+}
+
+function setup() {
+    count = 0
+    w = img.width;
+    h = img.height; 
+    createCanvas(w, h);
+    imageMode(CENTER);
+    noStroke();
+    background(255);
+    img.loadPixels();
+}
+
+function draw() {
+    frameRate = 20;
+    let x = random(img.width);
+    let y = random(img.height);
+    count++; // for the red dots 
+    let minecraft = map(mouseX, 0, width, 10,70);// generates the squares
+    let blood = map(mouseX, 0, width, 10,50); 
+
+    let sq = img.get(x, y);
+    if(count % 7 == 0) {
+        fill(200, 0, 0);
+        ellipse(x+20,y-30, blood, blood); 
+
+        console.log("yay!")
+    }
+        fill(sq, 0);
+    rect(x, y, minecraft, minecraft);
+  
 }
